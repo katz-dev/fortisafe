e3c8d88dbb6de89b39b04a76e1aab826fe77aeb8
Leo Gavin <71537398+DFanso@users.noreply.github.com>
2025-05-05 00:29:39 +0530

    change server port from 3000 to 8080

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -39,7 +39,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(8080);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
